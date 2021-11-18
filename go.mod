b691ce53af81b23455668cead7fa5f689aa6d94f
nabeelnoor <nabeelnoor914@gmail.com>
2021-11-18 18:38:34 +0500

    cors is setup and react is fetching

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module Rest/pk
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/nabeelnoor/assignment02IBC v1.0.0
+)
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main //purpose is to make rest api in golang
 
 import (
+	hbl "Rest/pk/BLHandler"
 	b "Rest/pk/Book"
 	hb "Rest/pk/Handlers"
 	"log"
@@ -14,12 +15,15 @@ func main() {
 	temp.Id = 123
 	router := mux.NewRouter()
 
+	router.HandleFunc("/bl", hbl.GetBlockChain).Methods(http.MethodGet) //get api
+
 	router.HandleFunc("/books", hb.GetAllBooks).Methods(http.MethodGet) //get api
 	// router.HandleFunc("/books/{id}", hb.GetBook).Methods(http.MethodGet) //get book by id ,, localhost:4000/1
 	router.HandleFunc("/books/id", hb.GetBook2).Methods(http.MethodGet)        //get book by id ,another way to do it  ,e.g such as localhost:4000/books/id?val=1&val2=23
 	router.HandleFunc("/addbooks", hb.AddBook).Methods(http.MethodPost)        //post api      method :post   localhost:4000/addbooks   body {		"title": "barium ",		 "author":"abcasdasd",		 "desc":"abc"	}
 	router.HandleFunc("/updatebook", hb.UpdateBook).Methods(http.MethodPut)    //update api   method:put 	   localhost:4000/updatebook?bookID=81   body {		"title": "barium update",		 "author":"up",		 "desc":"update- abc"	}
 	router.HandleFunc("/deletebook", hb.DeleteBook).Methods(http.MethodDelete) //delete api   method: delete   localhost:4000/deletebook?bookID=81
+
 	// router.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
 	// 	json.NewEncoder(w).Encode("Hello World")
 	// })
